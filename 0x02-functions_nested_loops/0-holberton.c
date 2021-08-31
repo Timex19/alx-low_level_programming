d4ad44389f1bc0f3f156853618f536e1d3af4fff
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-08-31 21:15:27 +0100

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,20 +1,19 @@
-#include "main.h"
+#include <stdio.h>
+#include "holberton.h"
 
 /**
- * main - Prints Holberton as a message.
- *
- * Return: Always 0 (Success)
+ * main - main function
+ * Description - Print combination fo two different numbers
+ * Return: 0
  */
 int main(void)
 {
-	int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-	int count, sz;
+	int i;
+	char hbtn[] = "Holberton\n";
 
-	sz = sizeof(str) / sizeof(int);
-	for (count = 0; count < sz; count++)
+	for (i = 0; i <= 10; i++)
 	{
-		_putchar(str[count]);
+		_putchar(hbtn[i]);
 	}
-	_putchar('\n');
 	return (0);
 }
