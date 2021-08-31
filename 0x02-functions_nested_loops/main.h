43b7c655ac53ef123120c8b65d306318092730e4
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-08-31 15:38:13 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,30 +1,13 @@
-#define HOLBERTON_H
+#include <unistd.h>
 
-/* _putchar.c */
-int _putchar(char);
-
-/*10 x alphabet */
-void print_alphabet(void);
-
-/* 2-print_alphabet_x10.c */
-void print_alphabet_x10(void);
-
-int _islower(int c);
-
-int _isalpha(int n);
-
-int print_sign(int n);
-
-int _abs(int n);
-
-int print_last_digit(int n);
-
-void jack_bauer(void);
-
-void times_table(void);
-
-int add(int a, int b);
-
-void print_to_98(int n);
-
-void print_times_table(int n);
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
