1322767bff48635f424948da30bb24ea8ce81db8
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-08-31 16:20:36 +0100

    Update main.h

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,12 +1,30 @@
-#include "main.h"
+#define MAIN_H
 
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    print_alphabet();
-    return (0);
-}
+/* _putchar.c */
+int _putchar(char);
+
+/*10 x alphabet */
+void print_alphabet(void);
+
+/* 2-print_alphabet_x10.c */
+void print_alphabet_x10(void);
+
+int _islower(int c);
+
+int _isalpha(int n);
+
+int print_sign(int n);
+
+int _abs(int n);
+
+int print_last_digit(int n);
+
+void jack_bauer(void);
+
+void times_table(void);
+
+int add(int a, int b);
+
+void print_to_98(int n);
+
+void print_times_table(int n);
