d99a47e03c8e3b7e046c1e94f26763a0a33a008b
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-08 12:51:09 +0100

    Fixing issue

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,19 +1,20 @@
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * list_len - number of elements list
+ * @h: pointer to structure
+ * Return: length.
  */
 
 size_t list_len(const list_t *h)
 {
-	size_t nelem;
 
-	nelem = 0;
+unsigned int i = 0;
+
 	while (h != NULL)
 	{
+		i++;
 		h = h->next;
-		nelem++;
 	}
-	return (nelem);
+	return (i);
 }
