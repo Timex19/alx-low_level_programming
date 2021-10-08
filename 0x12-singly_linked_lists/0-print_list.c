6ee35939d1dd32cc64efbb92cf98681d0cee9988
Ojo Timilehin Joseph <Ojooluwatimilehinpeter@yahoo.com>
2021-10-08 12:55:02 +0100

    Adding function that prints all the elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,26 @@
 #include "lists.h"
+
 /**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
+ * print_list - print elements of list
+ * @h: pointer to structure
+ * Return: length.
  */
+
+
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
 
-	nelem = 0;
+unsigned int i = 0;
+
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
-		else
+		if (h->str != NULL)
 			printf("[%d] %s\n", h->len, h->str);
+		else
+			printf("[0] (nil)\n");
 		h = h->next;
-		nelem++;
+		i++;
 	}
-	return (nelem);
+	return (i);
 }
