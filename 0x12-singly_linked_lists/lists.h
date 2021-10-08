b70441b5a797dcb5ef7950a29172cc633e8e17ae
Timex19 <ojooluwatimilehinpeter@yahoo.com>
2021-10-08 11:44:58 +0100

    Adding function that frees a linked list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,8 +1,10 @@
-#ifndef _list_h
-#define _list_h
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
